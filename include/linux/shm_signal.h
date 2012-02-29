3cfd7e60503fd9b56a55e3c95227e6b67ee84a75
Ira W. Snyder <iws@ovro.caltech.edu>
2012-02-28 20:51:42 -0500

    shm_signal: clearly specify endianness
    The shm_signal code uses structures which are designed to be shared between
    disparate systems, such as 32-bit and 64-bit, as well as Linux and Windows.
    
    Since shm_signal is primarily intended to be used by qemu/kvm, which
    supports virtual guests with a different CPU architecture than the host,
    clearly define an endianness for the shared structures.
    
    The endianness is defined to be little-endian, to avoid byte swapping in
    the most common case: x86.
    
    Signed-off-by: Ira W. Snyder <iws@ovro.caltech.edu>
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- include/linux/shm_signal.h
+++ include/linux/shm_signal.h
@@ -33,8 +33,8 @@
  *-----------
  */
 
-#define SHM_SIGNAL_MAGIC 0x58fa39df
-#define SHM_SIGNAL_VER   1
+#define SHM_SIGNAL_MAGIC cpu_to_le32(0x58fa39df)
+#define SHM_SIGNAL_VER   cpu_to_le32(1)
 
 struct shm_signal_irq {
 	__u8                  enabled;
@@ -48,8 +48,8 @@ enum shm_signal_locality {
 };
 
 struct shm_signal_desc {
-	__u32                 magic;
-	__u32                 ver;
+	__le32                 magic;
+	__le32                 ver;
 	struct shm_signal_irq irq[2];
 };
 
